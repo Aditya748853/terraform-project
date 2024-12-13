10cb4336b6f69639f197a81aeb5b9b07de8f634f
Aditya Anand <anandaditya701@gmail.com>
2024-12-13 11:47:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-module "vpc" {
-  source             = "./modules/vpc"
-  vpc_name           = "my-vpc"
-  cidr_block         = "10.0.0.0/16"
-  public_subnet_cidr = "10.0.1.0/24"
-  availability_zone  = "us-east-1a"
-}
-
-module "ec2_autoscaling" {
-  source                = "./modules/ec2_autoscaling"
-  subnet_id             = module.vpc.subnet_id
-  launch_template_name  = "my-launch-template"
-  ami_id                = "ami-0abcdef1234567890"
-  instance_type         = "t2.micro"
-  min_size              = 1
-  max_size              = 3
-  desired_capacity      = 2
-}
-
 module "lambda" {
   source           = "./modules/lambda"
   lambda_name      = "my-lambda"
